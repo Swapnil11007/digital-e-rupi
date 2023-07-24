33c9237187df0a248e7afc6257d35e1d22615f05
Swapnil11007 <shindes12121@gmail.com>
2023-07-24 11:09:53 +0530

    registartion page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,22 +20,25 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
+  // const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api')
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+  // useEffect(() => {
+  //   fetch('/api')
+  //     .then((response) => response.json())
+  //     .then((data) => setMessage(data.message))
+  //     .catch((error) => console.error('Error fetching data:', error));
+  // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello from the frontend!</p>
-        <p>{message}</p>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>Hello from the frontend!</p>
+    //     <p>{message}</p>
+    //   </header>
+    // </div>
+     <div align='center'>
+     <RegistrationForm />
+   </div>
   );
 }
 
